f53757528602ea278b5738d43bf66f13e1d9f4a4
Adam Lawson <dev@goshawk22.uk>
2025-10-11 12:49:23 +0100

    Fix bug with NaN colour in lineargradient

--- src/handlers/lineargradient.js
+++ src/handlers/lineargradient.js
@@ -101,10 +101,14 @@ export function LinearGradient() {
     // Generate gradient for each segment picking colors from palette
     for (let i = 0, data = this._data; i < data.length; i++) {
       if (i < data.length - 1) {
+        var value = 0;
         const dist = data[i + 1].dist - data[i].dist;
         const totalDist = data[data.length - 1].dist - data[0].dist;
         const offset = (data[i].dist - data[0].dist) / totalDist;
-        gradient.addColorStop(offset, palette.getRGBColor(data[i][attr]));
+        if (!isNaN(data[i][attr])) {
+          value = data[i][attr];
+        }
+        gradient.addColorStop(offset, palette.getRGBColor(value));
       }
     }
 
